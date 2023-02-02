85b707694eca39da8c8c9b135bfd19e242a0cccc
Thomas Allen <allen.thomas.c@gmail.com>
2023-02-02 11:48:46 -0800

    begin tracking tokenizer

---
+++ compiler_frontend/JackTokenizer.cpp
@@ -0,0 +1,189 @@
+#include "JackTokenizer.h"
+
+#include <cctype>
+#include <sstream>
+
+/* -------------------------------------------------------------------------- */
+
+JackTokenizer::JackTokenizer(const std::string& fileName) :
+        inFile(fileName),
+        fname(fileName),
+        currLine(0),
+        currColumnNum(0),
+        moreTokens(true),
+        currToken(),
+        currTokenType(),
+        currKeyword(),
+        currSymbol(),
+        currIdentifier(),
+        currIntVal(),
+        currStringVal() {
+    if (!inFile.is_open()) {
+        std::cerr << "ERROR: Could not open file \"" << fileName << "\"\n";
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+void JackTokenizer::Advance() {
+    // read new line if necessary and check for EOF
+    if (currLine.empty() || currColumnNum >= currLine.size()) {
+        ReadNextLine();
+        if (!inFile.good()) {
+            currTokenType = EMPTY;
+            return;
+        }
+    }
+
+    // skip spaces
+    while (currLine[currColumnNum] == ' ' || currLine[currColumnNum] == '\t') {
+        ++currColumnNum;
+
+        if (currColumnNum >= currLine.size()) {
+            currTokenType = SPACE;
+            ReadNextLine();
+            return;
+        }
+    }
+
+    // check for comments
+    if (currLine[currColumnNum] == '/' &&
+        (currColumnNum + 1) < currLine.size()) {
+        if (currLine[currColumnNum + 1] == '/') {
+            currTokenType = COMMENT;
+            ReadNextLine();
+        } else if (currLine[currColumnNum + 1] == '*') {
+            currTokenType = COMMENT;
+            currColumnNum += 2;
+            ParseDelimitedComment();
+        }
+    }
+
+    // check for symbols
+    else if (symbolSet.find(currLine[currColumnNum]) != symbolSet.end()) {
+        currToken = currLine[currColumnNum];
+        currSymbol = currLine[currColumnNum];
+        currTokenType = SYMBOL;
+        ++currColumnNum;
+    }
+
+    // check for string literals
+    else if (currLine[currColumnNum] == '"') {
+        ++currColumnNum;
+        ParseStringLiteral();
+    }
+
+    // check for int literals
+    else if (std::isdigit(currLine[currColumnNum])) {
+        ParseIntLiteral();
+    }
+
+    // parse keywords from identifiers
+    else {
+        ParseKeywordIdentifier();
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+void JackTokenizer::ReadNextLine() {
+    std::getline(inFile, currLine);
+    ++currLineNum;
+    currColumnNum = 0;
+
+    if (!inFile.good()) {
+        moreTokens = false;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+void JackTokenizer::ParseDelimitedComment() {
+    unsigned originalLineNum = currLineNum;
+
+    auto commentEnd = currLine.find(multiCommentCloser, currColumnNum);
+    while (commentEnd == std::string::npos) {
+        ReadNextLine();
+        ++currLineNum;
+        currColumnNum = 0;
+
+        // unexpected EOF
+        if (!inFile.good()) {
+            const std::string errMsg = "Unclosed multi-line comment";
+            tokErrHandler.Report(fname, originalLineNum, errMsg);
+        }
+
+        commentEnd = currLine.find(multiCommentCloser, currColumnNum);
+    }
+
+    currTokenType = COMMENT;
+    currColumnNum = commentEnd + 2;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void JackTokenizer::ParseStringLiteral() {
+    size_t startPos = currColumnNum;
+    size_t endQuotePos = currLine.find('"', currColumnNum);
+
+    if (endQuotePos == std::string::npos) {
+        const std::string errMsg = "Unclosed string literal";
+        tokErrHandler.Report(fname, currLineNum, errMsg);
+    }
+
+    currStringVal = currLine.substr(startPos, endQuotePos - startPos);
+    currToken = currStringVal;
+    currTokenType = STRING_CONST;
+
+    currColumnNum = endQuotePos + 1;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void JackTokenizer::ParseIntLiteral() {
+    std::stringstream intStream;
+
+    while (std::isdigit(currLine[currColumnNum])) {
+        intStream << currLine[currColumnNum];
+        ++currColumnNum;
+
+        if (currColumnNum >= currLine.size()) {
+            const std::string errMsg = "Missing semicolon after int literal";
+            tokErrHandler.Report(fname, currLineNum, currColumnNum, errMsg);
+        }
+    }
+
+    currToken = intStream.str();
+    currTokenType = INT_CONST;
+    currIntVal = std::stoi(intStream.str());
+}
+
+/* -------------------------------------------------------------------------- */
+
+void JackTokenizer::ParseKeywordIdentifier() {
+    std::stringstream textStream;
+
+    while (std::isalnum(currLine[currColumnNum]) ||
+           currLine[currColumnNum] == '_') {
+        textStream << currLine[currColumnNum];
+        ++currColumnNum;
+
+        if (currColumnNum >= currLine.size()) {
+            const std::string errMsg = "Missing semicolon";
+            tokErrHandler.Report(fname, currLineNum, currColumnNum, errMsg);
+        }
+    }
+
+    currToken = textStream.str();
+
+    if (keywordMap.find(currToken) != keywordMap.end()) {
+        currKeyword = keywordMap.at(currToken);
+
+    } else {
+        currTokenType = IDENTIFIER;
+        currIdentifier = currToken;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
---
+++ compiler_frontend/JackTokenizer.h
@@ -0,0 +1,129 @@
+#ifndef JACK_TOKENIZER_H
+#define JACK_TOKENIZER_H
+
+#include "ErrorHandler.h"
+
+#include <fstream>
+#include <map>
+#include <string>
+
+class JackTokenizer {
+  public:
+    JackTokenizer(const std::string& fileName);
+
+    // remove unwanted constructors
+    JackTokenizer(const JackTokenizer& that) = delete;
+    JackTokenizer(const JackTokenizer&& that) = delete;
+    JackTokenizer& operator=(const JackTokenizer& that) = delete;
+    JackTokenizer& operator=(const JackTokenizer&& that) = delete;
+
+    // enums for various program components
+    enum Token {
+        KEYWORD,
+        SYMBOL,
+        IDENTIFIER,
+        INT_CONST,
+        STRING_CONST,
+        COMMENT,
+        SPACE,
+        EMPTY,
+        UNKNOWN
+    };
+
+    enum Keyword {
+        CLASS,
+        METHOD,
+        FUNCTION,
+        CONSTRUCTOR,
+        INT,
+        BOOLEAN,
+        CHAR,
+        VOID,
+        VAR,
+        STATIC,
+        FIELD,
+        LET,
+        DO,
+        IF,
+        ELSE,
+        WHILE,
+        RETURN,
+        TRUE,
+        FALSE,
+        NULL_KEY,
+        THIS,
+        UNKNOWN
+    };
+
+    // public interface
+    bool HasMoreTokens() const { return moreTokens; }
+    void Advance();
+    Token TokenType() const { return currTokenType; }
+    std::string GetToken() const { return currToken; }  // temporary?
+    Keyword KeywordType() const { return currKeyword; }
+    char Symbol() const { return currSymbol; }
+    std::string Identifier() const { return currIdentifier; }
+    int IntVal() const { return currIntVal; }
+    std::string StringVal() const { return currStringVal; }
+    unsigned LineNum() const { return currLineNum; }
+    size_t ColNum() const { return currColumnNum; }
+
+    // data
+  private:
+    std::ifstream inFile;
+    std::string fname;
+    unsigned currLineNum;
+    size_t currColumnNum;
+
+    std::string currLine;
+
+    bool moreTokens;
+    std::string currToken;
+    Token currTokenType;
+    Keyword currKeyword;
+    char currSymbol;
+    std::string currIdentifier;
+    int currIntVal;
+    std::string currStringVal;
+
+    ErrorHandler tokErrHandler;
+
+    const std::string multiCommentCloser = "*/";
+
+    const std::set<char> symbolSet = {'{', '}', '(', ')', '[', ']', '.',
+                                      ',', ';', '+', '-', '*', '/', '&',
+                                      '|', '<', '>', '=', '~'};
+
+    const std::map<std::string, Keyword> keywordMap = {
+        {"class", CLASS},
+        {"method", METHOD},
+        {"function", FUNCTION},
+        {"constructor", CONSTRUCTOR},
+        {"int", INT},
+        {"boolean", BOOLEAN},
+        {"char", CHAR},
+        {"void", VOID},
+        {"var", VAR},
+        {"static", STATIC},
+        {"field", FIELD},
+        {"let", LET},
+        {"do", DO},
+        {"if", IF},
+        {"else", ELSE},
+        {"while", WHILE},
+        {"return", RETURN},
+        {"true", TRUE},
+        {"false", FALSE},
+        {"null", NULL_KEY},
+        {"this", THIS}};
+
+    // methods
+  private:
+    void ReadNextLine();
+    void ParseDelimitedComment();
+    void ParseStringLiteral();
+    void ParseIntLiteral();
+    void ParseKeywordIdentifier();
+};
+
+#endif /* JACK_TOKENIZER_H */

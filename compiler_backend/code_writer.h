97407d8dcaad08265830236b59719270f4dfcf30
Thomas Allen <allen.thomas.c@gmail.com>
2023-02-02 11:48:28 -0800

    begin using clang-format to lay out headers

--- compiler_backend/code_writer.h
+++ compiler_backend/code_writer.h
@@ -7,43 +7,43 @@
 #include <string>
 
 class CodeWriter {
-    public:
-        CodeWriter(const std::string & outName);
+  public:
+    CodeWriter(const std::string& outName);
 
-        // delete unwanted constructors
-        CodeWriter(const CodeWriter & that) = delete;
-        CodeWriter(const CodeWriter && that) = delete;
-        CodeWriter & operator=(const CodeWriter & that) = delete;
-        CodeWriter & operator=(const CodeWriter && that) = delete;
+    // delete unwanted constructors
+    CodeWriter(const CodeWriter& that) = delete;
+    CodeWriter(const CodeWriter&& that) = delete;
+    CodeWriter& operator=(const CodeWriter& that) = delete;
+    CodeWriter& operator=(const CodeWriter&& that) = delete;
 
-        void SetFileName(const std::string & fname) { infileName = fname; }
-        void WriteArithmetic(const std::string & command);
-        void WritePushPop(const Command ptype, const std::string & segment,
-                          const int index);
+    void SetFileName(const std::string& fname) { infileName = fname; }
+    void WriteArithmetic(const std::string& command);
+    void WritePushPop(const Command ptype, const std::string& segment,
+                      const int index);
 
-    private:
-        int jumpIndex;
-        std::ofstream outFile;
-        std::string infileName;
+  private:
+    int jumpIndex;
+    std::ofstream outFile;
+    std::string infileName;
 
-        const std::string pushCommand = "push";
-        const std::string popCommand = "pop";
-        const std::string constSegment = "constant";
+    const std::string pushCommand = "push";
+    const std::string popCommand = "pop";
+    const std::string constSegment = "constant";
 
-        const std::set<std::string> binaryCommands = {"add", "sub", "eq", "gt",
-                                                      "lt", "and", "or"};
+    const std::set<std::string> binaryCommands = {"add", "sub", "eq", "gt",
+                                                  "lt",  "and", "or"};
 
-        const std::set<std::string> unaryCommands = {"neg", "not"};
+    const std::set<std::string> unaryCommands = {"neg", "not"};
 
-        // methods
-        void WritePush(const std::string & segment, const int index);
-        void WritePop(const std::string & segment, const int index);
-        void WriteBinaryOp(const std::string & command);
-        void WriteUnaryOp(const std::string & command);
-        void WriteOpCommand(const std::string & command);
-        void PushRegister(const std::string & reg);
-        void PopRegister(const std::string & reg);
-        void WriteComparison(const std::string & op);
+    // methods
+    void WritePush(const std::string& segment, const int index);
+    void WritePop(const std::string& segment, const int index);
+    void WriteBinaryOp(const std::string& command);
+    void WriteUnaryOp(const std::string& command);
+    void WriteOpCommand(const std::string& command);
+    void PushRegister(const std::string& reg);
+    void PopRegister(const std::string& reg);
+    void WriteComparison(const std::string& op);
 };
 
 #endif /* CODE_WRITER_H */
--- compiler_backend/parser.h
+++ compiler_backend/parser.h
@@ -6,36 +6,45 @@
 #include <fstream>
 #include <string>
 
-enum class Command {    ARITHMETIC, PUSH, POP, LABEL,
-                        GOTO, IF, FUNCTION, RETURN,
-                        CALL, EMPTY, UNKNOWN  };
+enum class Command {
+    ARITHMETIC,
+    PUSH,
+    POP,
+    LABEL,
+    GOTO,
+    IF,
+    FUNCTION,
+    RETURN,
+    CALL,
+    EMPTY,
+    UNKNOWN
+};
 class Parser {
-    public:
-        Parser(const std::string & fileName);
-        // remove unwanted constructors
+  public:
+    Parser(const std::string& fileName);
+    // remove unwanted constructors
 
-        bool Advance();
-        Command CommandType();
-        std::string CommandText();
-        std::string FirstArg();
-        std::string SecondArg();
+    bool Advance();
+    Command CommandType();
+    std::string CommandText();
+    std::string FirstArg();
+    std::string SecondArg();
 
-    private:
-        std::ifstream inFile;
+  private:
+    std::ifstream inFile;
 
-        std::string currLine;
-        std::string command;
-        std::string arg1;
-        std::string arg2;
+    std::string currLine;
+    std::string command;
+    std::string arg1;
+    std::string arg2;
 
-        boost::char_separator<char> sep(" \t\r\n");
+    boost::char_separator<char> sep(" \t\r\n");
 
-        const std::string commentInitializer = "//";
-        const std::set<std::string> arithmeticCommands = {"add", "sub", "eq",
-                                                          "gt", "lt", "and",
-                                                          "or", "neg", "not"};
-        const std::string pushCommand = "push";
-        const std::string popCommand = "pop";
+    const std::string commentInitializer = "//";
+    const std::set<std::string> arithmeticCommands = {
+            "add", "sub", "eq", "gt", "lt", "and", "or", "neg", "not"};
+    const std::string pushCommand = "push";
+    const std::string popCommand = "pop";
 };
 
 #endif /* PARSER_H */

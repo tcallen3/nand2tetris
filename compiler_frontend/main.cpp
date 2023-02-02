2cd7df13d58d282096e9c98eff288d0ae38a3947
Thomas Allen <allen.thomas.c@gmail.com>
2023-02-02 11:48:48 -0800

    begin tracking top-level compiler manager

---
+++ compiler_frontend/main.cpp
@@ -0,0 +1,52 @@
+#include "CompilationEngine.h"
+
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+const std::string inExt = ".jack";
+const std::string outExt = ".vm";
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <.jack file or directory>\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    fs::path inputPath(argv[1]);
+    std::string outName = "";
+
+    if (fs::exists(inputPath)) {
+        if (fs::is_regular_file(inputPath) && inputPath.extension() == inExt) {
+            outName = inputPath.stem();
+            outName += outExt;
+
+            CompilationEngine compiler(inputPath.filename(), outName);
+
+            compiler.Compile();
+
+        } else if (fs::is_directory(inputPath)) {
+
+            for (auto& p : fs::directory_iterator(inputPath)) {
+                if (p.extension() == inExt) {
+                    fs::path currFilePath = p.parent_path() / p.stem();
+                    std::string outName = currFilePath.string() + outExt;
+
+                    CompilationEngine compiler(p.string(), outName);
+
+                    compiler.Compile();
+                }
+            }
+
+        } else {
+            std::cerr << "ERROR: Unsupported file type for " << inputPath
+                      << '\n';
+        }
+
+    } else {
+        std::cerr << inputPath << " does not exist\n";
+    }
+
+    return 0;
+}

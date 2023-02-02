006ce764fce8bf1f94d94e73478c06c7b3171f8f
Thomas Allen <allen.thomas.c@gmail.com>
2023-02-02 11:49:05 -0800

    begin tracking VM command writer

---
+++ compiler_frontend/VMWriter.cpp
@@ -0,0 +1,66 @@
+#include "VMWriter.h"
+
+#include <iostream>
+
+/* -------------------------------------------------------------------------- */
+
+VMWriter::VMWriter(const std::string& outName) : outFile(outName) {
+    if (!outFile.is_open()) {
+        std::cerr << "ERROR: Could not open file \"" << outName << "\"\n";
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WritePush(const Segment segment, const int index) {
+    outFile << "push " << segNames.at(segment) << ' ' << index << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WritePop(const Segment segment, const int index) {
+    outFile << "pop " << segNames.at(segment) << ' ' << index << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteArithmetic(const Command command) {
+    outFile << commandNames.at(command) << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteLabel(const std::string& label) {
+    outFile << "label " << label << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteGoto(const std::string& label) {
+    outFile << "goto " << label << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteIf(const std::string& label) {
+    outFile << "if-goto " << label << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteCall(const std::string& name, const int nArgs) {
+    outFile << "call " << name << ' ' << nArgs << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteFunction(const std::string& name, const int nLocals) {
+    outFile << "function " << name << ' ' << nLocals << '\n';
+}
+
+/* -------------------------------------------------------------------------- */
+
+void VMWriter::WriteReturn() { outFile << "return\n"; }
+
+/* -------------------------------------------------------------------------- */
---
+++ compiler_frontend/VMWriter.h
@@ -0,0 +1,57 @@
+#ifndef VM_WRITER_H
+#define VM_WRITER_H
+
+#include <fstream>
+#include <map>
+#include <string>
+
+class VMWriter {
+  public:
+    VMWriter(const std::string& outName);
+
+    // remove unwanted constructors
+    VMWriter(const VMWriter& that) = delete;
+    VMWriter(const VMWriter&& that) = delete;
+    VMWriter& operator=(const VMWriter& that) = delete;
+    VMWriter& operator=(const VMWriter&& that) = delete;
+
+    enum Segment { CONST, ARG, LOCAL, STATIC, THIS, THAT, POINTER, TEMP };
+    const std::map<Segment, std::string> segNames = {
+        {CONST, "constant"},  {ARG, "argument"}, {LOCAL, "local"},
+        {STATIC, "static"},   {THIS, "this"},    {THAT, "that"},
+        {POINTER, "pointer"}, {TEMP, "temp"}};
+
+    enum Command { ADD, SUB, MULT, DIV, NEG, EQ, GT, LT, AND, OR, NOT };
+    const std::map<Command, std::string> commandNames = {
+        {ADD, "add"},
+        {SUB, "sub"},
+        {MULT, "call Math.multiply 2"},
+        {DIV, "call Math.divide 2"},
+        {NEG, "neg"},
+        {EQ, "eq"},
+        {GT, "gt"},
+        {LT, "lt"},
+        {AND, "and"},
+        {OR, "or"},
+        {NOT, "not"}};
+
+    void WritePush(const Segment segment, const int index);
+    void WritePop(const Segment segment, const int index);
+
+    void WriteArithmetic(const Command command);
+
+    void WriteLabel(const std::string& label);
+    void WriteGoto(const std::string& label);
+    void WriteIf(const std::string& label);
+
+    void WriteCall(const std::string& name, const int nArgs);
+    void WriteFunction(const std::string& name, const int nLocals);
+
+    void WriteReturn();
+
+    // data
+  private:
+    std::ofstream outFile;
+};
+
+#endif /* VM_WRITER_H */
